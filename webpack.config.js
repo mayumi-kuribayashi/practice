9c4f865c1994fd84a2c08138ada35efb74ea5648
kuribayashi000 <kuribayashi@3kaku.co.jp>
2019-11-29 19:58:20 +0900

    webpack config 作成

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+const src = path.join(__dirname, 'src');
+const dist = path.join(__dirname, 'dist');
+
+console.log(src)
+module.exports = {
+  mode: 'development', //devel モードで実行します
+  entry: path.resolve(src, 'js/main.js'), //ビルドを実行するファイルパス
+  output: {
+    filename: 'bundle.js', //育成されるファイル名
+    path: dist // 育成先のディレクトリ
+  },
+  resolve: {
+    modules: ['node_modules'], //import 文のパス指定にnode_modulesを省略できるようにします
+    extensions: ['.js', 'jsx'] //.js またはjsxの拡張子を省略できるようにします
+  },
+  module: {
+    rules: [{
+      test: /\.(js|jsx)$/, // ルールを適用するファイルの正規表現
+      exclude: /node_modules/, // node_modules以下のファイルには適応しないようにする
+      loader: 'babel-loader' //使用するloader
+    }]
+  }
+}
